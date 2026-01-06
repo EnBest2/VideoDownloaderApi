5df54247961b51037bd3efe504bdd38476d2c6d5
EnBest2 <enbest2@gmail.com>
2026-01-06 09:57:57 +0100

    Implement Google Drive file uploader class

---
+++ GoogleDriveUploader.cs
@@ -0,0 +1,50 @@
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+
+public class GoogleDriveUploader
+{
+    private readonly DriveService _driveService;
+
+    public GoogleDriveUploader(string jsonCredentials)
+    {
+        var credential = GoogleCredential.FromJson(jsonCredentials)
+            .CreateScoped(DriveService.ScopeConstants.Drive);
+
+        _driveService = new DriveService(new BaseClientService.Initializer
+        {
+            HttpClientInitializer = credential,
+            ApplicationName = "VideoDownloader"
+        });
+    }
+
+    public async Task<string> UploadFileAsync(string filePath, string fileName)
+    {
+        var fileMetadata = new Google.Apis.Drive.v3.Data.File
+        {
+            Name = fileName,
+            Parents = new[] { "root" }
+        };
+
+        using var stream = new FileStream(filePath, FileMode.Open);
+
+        var request = _driveService.Files.Create(fileMetadata, stream, "video/mp4");
+        request.Fields = "id";
+        var result = await request.UploadAsync();
+
+        if (result.Status != Google.Apis.Upload.UploadStatus.Completed)
+            throw new Exception("Upload failed");
+
+        var fileId = request.ResponseBody.Id;
+
+        var permission = new Google.Apis.Drive.v3.Data.Permission
+        {
+            Role = "reader",
+            Type = "anyone"
+        };
+
+        await _driveService.Permissions.Create(permission, fileId).ExecuteAsync();
+
+        return $"https://drive.google.com/uc?export=download&id={fileId}";
+    }
+}
