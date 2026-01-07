1f98246992cafa58f920fd5176a23dd3e582d436
EnBest2 <enbest2@gmail.com>
2026-01-07 10:24:24 +0100

    Implement BackgroundWorker for task processing

---
+++ Services/BackgroundWorker.cs
@@ -0,0 +1,18 @@
+public class BackgroundWorker : BackgroundService
+{
+    private readonly IBackgroundTaskQueue _taskQueue;
+
+    public BackgroundWorker(IBackgroundTaskQueue taskQueue)
+    {
+        _taskQueue = taskQueue;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            var workItem = await _taskQueue.DequeueAsync(stoppingToken);
+            await workItem(stoppingToken);
+        }
+    }
+}
